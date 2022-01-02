d423c34a5dd82ffcd4285a88e0994895d63a4f5a
Venilton FalvoJr <falvojr@gmail.com>
2022-01-02 12:42:22 -0300

    Finalização da tela de detalhes

--- app/src/main/java/me/dio/simulador/draft/ui/DetailActivity.kt
+++ app/src/main/java/me/dio/simulador/draft/ui/DetailActivity.kt
@@ -29,7 +29,21 @@ class DetailActivity : AppCompatActivity() {
                 Glide.with(this).load(match.place.image).into(binding.ivPlace)
                 supportActionBar?.title = match.place.name
 
-                //TODO Preencher demais atributos...
+                binding.tvDescription.text = match.description
+
+                Glide.with(this).load(match.homeTeam.image).into(binding.ivHomeTeam)
+                binding.tvHomeTeamName.text = match.homeTeam.name
+                binding.rbHomeTeamStars.rating = match.homeTeam.stars.toFloat()
+                if (match.homeTeam.score != null) {
+                    binding.tvHomeTeamScore.text = match.homeTeam.score.toString()
+                }
+
+                Glide.with(this).load(match.awayTeam.image).into(binding.ivAwayTeam)
+                binding.tvAwayTeamName.text = match.awayTeam.name
+                binding.rbAwayTeamStars.rating = match.awayTeam.stars.toFloat()
+                if (match.awayTeam.score != null) {
+                    binding.tvAwayTeamScore.text = match.awayTeam.score.toString()
+                }
             }
         }
     }
